4a3970db9d6ca8b303965addafbded3e6f64c4a3
Barrabin Fc. ⚑ <barrabin.fc@gmail.com>
2014-06-25 04:52:06 -0300

    Added underscore as deps

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+    api.use('underscore')
     api.add_files(["lib/machina/lib/machina.js"], ["client",'server']);
     api.export('machina')
 });
