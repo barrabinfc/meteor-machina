7c72973c3da49be45a39f73c4d927d91603603a5
Barrabin Fc. ⚑ <barrabin.fc@gmail.com>
2014-06-25 04:49:25 -0300

    Removing npm, using submodule

--- package.js
+++ package.js
@@ -2,12 +2,7 @@ Package.describe({
     summary: "A library for creating powerful and flexible finite state machines.  Loosely inspired by Erlang/OTP\'s gen_fsm behavior."
 });
 
-Npm.depends({
-    'underscore': '1.5.2'
-});
-
 Package.on_use(function (api) {
-    api.use('machina','client');
-    api.add_files(["machina.js"], "server");
-    api.add_files(["lib/machina/lib/machina.js"], "client");
+    api.add_files(["lib/machina/lib/machina.js"], ["client",'server']);
+    api.export('machina')
 });
